a2ff1ea0e92555583f181503ab00d02721e8fa12
polatoja <127290541+polatoja@users.noreply.github.com>
2025-04-21 10:45:52 +0200

    line

---
+++ Rasterization/Rasterization/CustomLine.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Rasterization
+{
+    public class CustomLine
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+        public int Thickness { get; set; } = 1;
+        public List<Rectangle> Pixels { get; set; } = new();
+
+        public void Draw(Canvas canvas)
+        {
+            Pixels.Clear();
+
+            int x0 = (int)Start.X;
+            int y0 = (int)Start.Y;
+            int x1 = (int)End.X;
+            int y1 = (int)End.Y;
+
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+
+            int x = x0;
+            int y = y0;
+
+            int xStep = dx >= 0 ? 1 : -1;
+            int yStep = dy >= 0 ? 1 : -1;
+
+            dx = Math.Abs(dx);
+            dy = Math.Abs(dy);
+
+            if (dx >= dy)
+            {
+                int d = 2 * dy - dx;
+                int incrE = 2 * dy;
+                int incrNE = 2 * (dy - dx);
+
+                DrawPixel(canvas, x, y);
+
+                for (int i = 0; i < dx; i++)
+                {
+                    if (d <= 0)
+                    {
+                        d += incrE;
+                        x += xStep;
+                    }
+                    else
+                    {
+                        d += incrNE;
+                        x += xStep;
+                        y += yStep;
+                    }
+                    DrawPixel(canvas, x, y);
+                }
+            }
+            else
+            {
+                int d = 2 * dx - dy;
+                int incrN = 2 * dx;
+                int incrNE = 2 * (dx - dy);
+
+                DrawPixel(canvas, x, y);
+
+                for (int i = 0; i < dy; i++)
+                {
+                    if (d <= 0)
+                    {
+                        d += incrN;
+                        y += yStep;
+                    }
+                    else
+                    {
+                        d += incrNE;
+                        x += xStep;
+                        y += yStep;
+                    }
+                    DrawPixel(canvas, x, y);
+                }
+            }
+        }
+
+
+        public void Redraw(Canvas canvas)
+        {
+            Remove(canvas);
+            Draw(canvas);
+        }
+
+        public void Remove(Canvas canvas)
+        {
+            foreach (var pixel in Pixels)
+                canvas.Children.Remove(pixel);
+            Pixels.Clear();
+        }
+
+
+        private void DrawPixel(Canvas canvas, int x, int y)
+        {
+            Rectangle rect = new Rectangle
+            {
+                Width = Thickness,
+                Height = Thickness,
+                Fill = Brushes.Black
+            };
+            Canvas.SetLeft(rect, x - Thickness / 2.0);
+            Canvas.SetTop(rect, y - Thickness / 2.0);
+            canvas.Children.Add(rect);
+            Pixels.Add(rect);
+        }
+
+
+        public bool IsNearPoint(Point p1, Point p2, double radius = 20)
+        {
+            return (p1 - p2).Length <= radius;
+        }
+
+        public bool IsNearLine(Point p)
+        {
+            return Pixels.Any(px =>
+            {
+                double x = Canvas.GetLeft(px);
+                double y = Canvas.GetTop(px);
+                return Math.Abs(x - p.X) < 5 && Math.Abs(y - p.Y) < 5;
+            });
+        }
+
+    }
+}
---
+++ Rasterization/Rasterization/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Rasterization
+{
+    public partial class MainWindow : Window
+    {
+        private List<CustomLine> lines = new();
+        private Point? currentStartPoint = null;
+        private CustomLine selectedLine = null;
+
+        // LINE
+        private bool isDrawingLine = false;
+        private bool isEditingLine = false;
+        private bool isDraggingStartLine = false;
+        private bool isDraggingEndLine = false;
+        private bool isDeletingLine = false;
+
+        // CIRCLE
+        private bool isDrawingCircle = false;
+        private bool isEditingCircle = false;
+        private bool isDraggingStartCircle = false;
+        private bool isDraggingEndCircle = false;
+        private bool isDeletingCircle = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawingCanvas.MouseLeftButtonDown += Canvas_MouseLeftButtonDown;
+            DrawingCanvas.MouseRightButtonDown += Canvas_MouseRightButtonDown;
+            DrawingCanvas.MouseMove += Canvas_MouseMove;
+            DrawingCanvas.MouseMove += Canvas_MouseMove;
+        }
+
+        private void DrawLine_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawingLine = true;
+            isEditingLine = false;
+            isDeletingLine = false;
+            currentStartPoint = null;
+            selectedLine = null;
+        }
+        private void EditLine_Click(object sender, RoutedEventArgs e)
+        {
+            isEditingLine = true;
+            isDeletingLine = false;
+            isDrawingLine = false;
+            selectedLine = null;
+        }
+        private void DeleteLine_Click(object sender, RoutedEventArgs e)
+        {
+            isDeletingLine = true;
+            isDrawingLine = false;
+            isEditingLine = false;
+            selectedLine = null;
+        }
+
+        private void DrawCircle_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawingCircle = true;
+            isEditingCircle = false;
+            isDeletingCircle = false;
+            currentStartPoint = null;
+        }
+
+        private void EditCircle_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawingCircle = false;
+            isEditingCircle = true;
+            isDeletingCircle = false;
+            currentStartPoint = null;
+        }
+
+        private void DeleteCircle_Click(object sender, RoutedEventArgs e)
+        {
+            isDrawingCircle = false;
+            isEditingCircle = false;
+            isDeletingCircle = true;
+            currentStartPoint = null;
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point click = e.GetPosition(DrawingCanvas);
+
+            if ((isDraggingStartLine || isDraggingEndLine) && selectedLine != null)
+            {
+                if (!lines.Contains(selectedLine))
+                {
+                    lines.Add(selectedLine);
+                }
+
+                MessageBox.Show("Line edited.");
+                isDraggingStartLine = false;
+                isDraggingEndLine = false;
+                selectedLine = null;
+                isEditingLine = false;
+            }
+            if (isDrawingLine)
+            {
+                if (currentStartPoint == null)
+                {
+                    currentStartPoint = click;
+                }
+                else
+                {
+                    var newLine = new CustomLine
+                    {
+                        Start = currentStartPoint.Value,
+                        End = click,
+                        Thickness = 1
+                    };
+                    newLine.Draw(DrawingCanvas);
+                    lines.Add(newLine);
+
+                    currentStartPoint = null;
+                }
+                return;
+            }
+            if (isEditingLine)
+            {
+                foreach (var line in lines)
+                {
+                    if (line.IsNearLine(click))
+                    {
+                        if (line.IsNearPoint(click, line.Start))
+                        {
+                            selectedLine = line;
+                            isDraggingStartLine = true;
+                            return;
+                        }
+                        else if (line.IsNearPoint(click, line.End))
+                        {
+                            selectedLine = line;
+                            isDraggingEndLine = true;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedLine != null && (isDraggingStartLine || isDraggingEndLine))
+            {
+                Point pos = e.GetPosition(DrawingCanvas);
+                if (isDraggingStartLine)
+                    selectedLine.Start = pos;
+                else if (isDraggingEndLine)
+                    selectedLine.End = pos;
+
+                selectedLine.Redraw(DrawingCanvas);
+            }
+        }
+
+        private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point click = e.GetPosition(DrawingCanvas);
+
+            if (isDeletingLine)
+            {
+                foreach (var line in lines.ToList())
+                {
+                    if (line.IsNearLine(click))
+                    {
+                        line.Remove(DrawingCanvas);
+                        lines.Remove(line);
+                        return;
+                    }
+                }
+
+                foreach (var line in lines)
+                {
+                    if (line.IsNearLine(click))
+                    {
+                        line.Thickness += 1;
+                        line.Redraw(DrawingCanvas);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+}
